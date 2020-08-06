45ba8808f9962f522a5b85b2bd0b7d4cf24d2e71
Tim Matthews <tim@3sidedcube.com>
2020-08-06 11:23:20 +0100

    Upgrade to SDK 30

--- library/src/main/java/com/cube/storm/util/lib/processor/GsonProcessor.java
+++ library/src/main/java/com/cube/storm/util/lib/processor/GsonProcessor.java
@@ -1,7 +1,7 @@
 package com.cube.storm.util.lib.processor;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
